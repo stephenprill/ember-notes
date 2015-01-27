7fc03ccf44456f5bf9174ee8c13046133af209ee
Stephen Prill <stephen.prill@gmail.com>
2015-01-27 11:24:58 -0700

    added flashMessage and validations for not being able to add blank note

--- app/controllers/notes.js
+++ app/controllers/notes.js
@@ -5,15 +5,33 @@ export default Ember.ArrayController.extend({
     newNote: function() {
       var body = this.get('noteCopy'),
       title = this.get('noteTitle');
-      var note = this.store.createRecord('note', { body: body, title: title });
-      this.set('noteCopy', '');
-      note.save();
+      if (body.trim) {
+        var note = this.store.createRecord('note', { body: body, title: title });
+        this.set('noteCopy', '');
+        note.save();
+      } else
+      return
     },
+
     deleteNote: function(id) {
-      this.store.find('note', id).then(function(note){
+      var self = this;
+      self.store.find('note', id).then(function(note){
         note.deleteRecord();
         note.save();
+        self.flashMessage('success', 'Congratulations! Your shit has been deleted.');
       });
+    },
+
+    deleted: function() {
+      var _this = this;
+
+      this.get('note').save().then(function() {
+        _this.formDeleted();
+      });
+    },
+    formDeleted: function() {
+      this.flashMessage('success', 'Congratulations! Your changes have been saved');
     }
+
   }
 });
