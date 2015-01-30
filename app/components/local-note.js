2956df9a0682c36486e3078de6012b741197ec3b
Stephen Prill <stephen.prill@gmail.com>
2015-01-30 10:59:42 -0700

    finished adding local-note component to point it functions but still need to figure out delete bug

--- app/components/local-note.js
+++ app/components/local-note.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  tagName: 'article',
+  classNames: ['note'],
+
+  actions: {
+    deleteNote: function(id) {
+      this.sendAction('deleteNote', id);
+    }
+  }
 });
